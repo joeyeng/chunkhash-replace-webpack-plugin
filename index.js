ab4e9380da23e9b68dc9bb7a86d123a92df800c9
Christopher Ehrlich <christopher.ehrlich@kompany.com>
2023-06-19 16:42:10 +0200

    webpack 4+ compatibility

--- index.js
+++ index.js
@@ -2,29 +2,27 @@
 
 const path = require('path');
 const fs = require('fs');
-const loaderUtils = require('loader-utils');
-const transform = require('./transform');
+const transform = require("./transform");
 
-function ChunkHashReplacePlugin(options) {
-  this.src = options.src;
-  this.dest = options.dest;
-}
+class ChunkHashReplacePlugin {
+  constructor(options) {
+    this.src = options.src;
+    this.dest = options.dest;
+  }
 
-ChunkHashReplacePlugin.prototype.apply = function (compiler) {
-  const self = this;
-  const folder = compiler.options.context;
-  const src = path.join(folder, self.src);
-  const dest = path.join(folder, self.dest);
-  const cssFilePaths = self.cssFilePaths;
+  apply(compiler) {
+    const self = this;
+    const folder = compiler.options.context;
+    const src = path.join(folder, self.src);
+    const dest = path.join(folder, self.dest);
 
-  fs.readFile(src, 'utf8', function (err, data) {
-    compiler.plugin('done', function (statsData) {
+    compiler.hooks.done.tap('ChunkHashReplacePlugin', (statsData) => {
       const stats = statsData.toJson();
       const template = fs.readFileSync(src, 'utf8');
       const htmlOutput = transform(template, stats.chunks);
       fs.writeFileSync(dest, htmlOutput);
     });
-  });
-};
+  }
+}
 
 module.exports = ChunkHashReplacePlugin;
